8772618c4a4507badd58bf2eb2d331676185fc6e
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2022-06-10 14:56:27 +0530

    Add files via upload

---
+++ priorityQueue_min_heap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+int main()
+{  
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+    pq.push(5);
+    pq.push(3);
+    pq.push(10);
+    pq.push(4);
+    while (!pq.empty())
+    {
+        std::cout << pq.top()<< std::endl;
+        pq.pop();
+    }
+    return 0;
+}
